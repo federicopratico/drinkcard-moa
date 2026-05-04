d9d8fba17a92d67725fd33ab8ae170686379c68d
federicopratico <federico.pratico@icloud.com>
2026-05-04 19:44:35 +0200

    feature: Add DomainException mother class and ExceptionHandler definition

---
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/shared/exception/DomainException.java
@@ -0,0 +1,7 @@
+package cat.itacademy.s04.t02.n02.drinkcardmoa.shared.exception;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
