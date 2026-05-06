88d8dc2100aed7d5d4790729c6a64c92d505a889
federicopratico <federico.pratico@icloud.com>
2026-05-06 11:02:34 +0200

    refactor: Add methods to security out port

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/application/port/out/PasswordEncoder.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/application/port/out/PasswordEncoder.java
@@ -2,5 +2,5 @@ package cat.itacademy.s04.t02.n02.drinkcardmoa.iam.application.port.out;
 
 public interface PasswordEncoder {
     String encode(String rawPassword);
-    boolean match(String rawPassword, String encodedPassword);
+    boolean matches(String rawPassword, String encodedPassword);
 }
