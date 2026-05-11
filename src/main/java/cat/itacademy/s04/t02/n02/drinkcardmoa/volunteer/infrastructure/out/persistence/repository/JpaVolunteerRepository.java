9ce6b32bde71233e885d0aa31a5867c1f7c4d9d3
federicopratico <federico.pratico@icloud.com>
2026-05-11 10:09:09 +0200

    feature: Add volunteer repository port and implementation

---
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/infrastructure/out/persistence/repository/JpaVolunteerRepository.java
@@ -0,0 +1,11 @@
+package cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.infrastructure.out.persistence.repository;
+
+import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.infrastructure.out.persistence.entity.VolunteerJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaVolunteerRepository extends JpaRepository <VolunteerJpaEntity, String> {
+    Optional<VolunteerJpaEntity> findByVolunteerId(String volunteerID);
+    boolean existsByVolunteerId(String volunteerID);
+}
