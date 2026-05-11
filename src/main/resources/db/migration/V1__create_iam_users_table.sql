1a87475f8b1364cd155a7a0363324ed8a4ccc4fd
federicopratico <federico.pratico@icloud.com>
2026-05-11 20:49:46 +0200

    chore: Refactor SQL schemas creation
    remove NOT NULL constraint to last_purchased attribute in volunteers table

--- src/main/resources/db/migration/V1__create_iam_users_table.sql
+++ src/main/resources/db/migration/V1__create_iam_users_table.sql
@@ -5,4 +5,12 @@ CREATE TABLE users(
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL
-)
+);
+
+INSERT INTO users (id, first_name, last_name, email, password, role)
+VALUES ('8799df50-d517-4693-9e46-51b537c305a2',
+        'System',
+        'Admin',
+        'admin@drinkcard.local',
+        '$2a$15$R8.9MKUADrMt8zbNAETFLOqwD3I7.bMoYCH4qaj8xOLOB/1LzL9ym',
+        'ADMIN');
