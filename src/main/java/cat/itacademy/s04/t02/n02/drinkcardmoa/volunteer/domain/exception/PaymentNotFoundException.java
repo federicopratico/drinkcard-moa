b41fc49525f8fd6e6ea9ab1e1652b15761be80af
federicopratico <federico.pratico@icloud.com>
2026-05-12 23:06:45 +0200

    feature: Add domain exceptions and dedicated methods in GlobalExceptionHandler

---
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/domain/exception/PaymentNotFoundException.java
@@ -0,0 +1,9 @@
+package cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.domain.exception;
+
+import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.exception.DomainException;
+
+public class PaymentNotFoundException extends DomainException {
+    public PaymentNotFoundException(String message) {
+        super(message);
+    }
+}
