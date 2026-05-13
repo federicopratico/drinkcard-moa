cae7a60cf951a7ceb98854aa49c919717fbd4447
federicopratico <federico.pratico@icloud.com>
2026-05-13 19:21:26 +0200

    refactor: Remove from VolunteerBeanConfig Payment configurations

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/infrastructure/config/VolunteerBeanConfiguration.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/infrastructure/config/VolunteerBeanConfiguration.java
@@ -1,14 +1,7 @@
 package cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.infrastructure.config;
 
-import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.in.usecase.ConfirmPaymentUseCase;
-import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.in.usecase.CreatePaymentCheckoutUseCase;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.in.usecase.CreateVolunteerUseCase;
-import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.out.EventPublisher;
-import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.out.PaymentRepository;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.out.VolunteerRepository;
-import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.out.payment.PaymentGateway;
-import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.service.ConfirmPaymentService;
-import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.service.CreatePaymentCheckoutService;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.service.CreateVolunteerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,20 +12,5 @@ public class VolunteerBeanConfiguration {
     @Bean
     CreateVolunteerUseCase createVolunteerUseCase(VolunteerRepository volunteerRepository) {
         return new CreateVolunteerService(volunteerRepository);
-    }
-
-    @Bean
-    CreatePaymentCheckoutUseCase createPaymentCheckoutUseCase(VolunteerRepository repository, PaymentGateway paymentGateway, PaymentRepository paymentRepository) {
-        return new CreatePaymentCheckoutService(
-                paymentRepository,
-                paymentGateway,
-                repository
-        );
-    }
-
-    @Bean
-    ConfirmPaymentUseCase confirmPaymentUseCase(
-            PaymentGateway paymentGateway, PaymentRepository paymentRepository, EventPublisher eventPublisher, VolunteerRepository volunteerRepository) {
-        return new ConfirmPaymentService(paymentGateway, paymentRepository, eventPublisher, volunteerRepository);
     }
 }
