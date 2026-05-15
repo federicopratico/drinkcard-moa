fef3065f6120b4fa1ece81ae6dd80a4a2c7aa3ba
federicopratico <federico.pratico@icloud.com>
2026-05-15 18:24:20 +0200

    chore: Add configuration classes for payments usecases and sumup adapter

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/infrastructure/config/PaymentBeanConfiguration.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/infrastructure/config/PaymentBeanConfiguration.java
@@ -8,26 +8,24 @@ import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.out.Vol
 import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.port.out.payment.PaymentGateway;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.service.ConfirmPaymentService;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.application.service.CreatePaymentCheckoutService;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.infrastructure.adapter.out.payment.SumUpPaymentAdapter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestClient;
 
 @Configuration
-@EnableConfigurationProperties(SumUpProperties.class)
+@EnableConfigurationProperties({SumUpProperties.class, PaymentProperties.class})
 public class PaymentBeanConfiguration {
 
     @Bean
-    CreatePaymentCheckoutUseCase createPaymentCheckoutUseCase(VolunteerRepository repository, PaymentGateway paymentGateway, PaymentRepository paymentRepository) {
-        return new CreatePaymentCheckoutService(
-                paymentRepository,
-                paymentGateway,
-                repository
-        );
-    }
+    PaymentGateway paymentGateway(SumUpProperties properties) {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(properties.getBaseUrl())
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + properties.getApiKey())
+                .build();
 
-    @Bean
-    ConfirmPaymentUseCase confirmPaymentUseCase(
-            PaymentGateway paymentGateway, PaymentRepository paymentRepository, EventPublisher eventPublisher, VolunteerRepository volunteerRepository) {
-        return new ConfirmPaymentService(paymentGateway, paymentRepository, eventPublisher, volunteerRepository);
+        return new SumUpPaymentAdapter(restClient, properties);
     }
 }
