5b0bb574314c6cbe0160fb03c48209b63b36e731
federicopratico <federico.pratico@icloud.com>
2026-05-16 16:57:11 +0200

    refactor: Make InsufficientcreditsException to extend DomainException

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/domain/exception/InsufficientCreditsException.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/volunteer/domain/exception/InsufficientCreditsException.java
@@ -1,6 +1,8 @@
 package cat.itacademy.s04.t02.n02.drinkcardmoa.volunteer.domain.exception;
 
-public class InsufficientCreditsException extends RuntimeException {
+import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.exception.DomainException;
+
+public class InsufficientCreditsException extends DomainException {
     public InsufficientCreditsException(String message) {
         super(message);
     }
