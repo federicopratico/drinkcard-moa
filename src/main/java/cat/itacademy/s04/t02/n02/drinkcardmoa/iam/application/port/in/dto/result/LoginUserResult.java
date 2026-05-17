ce300d53155f431183c20c5e6e9c9835622f67e9
federicopratico <federico.pratico@icloud.com>
2026-05-17 12:26:32 +0200

    refactor: Add volunteerId in the login response

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/application/port/in/dto/result/LoginUserResult.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/application/port/in/dto/result/LoginUserResult.java
@@ -4,11 +4,12 @@ import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.domain.model.aggregate.User;
 
 public record LoginUserResult(
         String token,
+        String volunteerId,
         String email,
         String role
 )
 {
     public static LoginUserResult from(User user, String token) {
-        return new LoginUserResult(token, user.getEmail().asString(), user.getRole().name());
+        return new LoginUserResult(token, user.getId().asString() , user.getEmail().asString(), user.getRole().name());
     }
 }
--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/infrastructure/adapter/in/rest/dto/response/LoginResponse.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/infrastructure/adapter/in/rest/dto/response/LoginResponse.java
@@ -2,6 +2,7 @@ package cat.itacademy.s04.t02.n02.drinkcardmoa.iam.infrastructure.adapter.in.res
 
 public record LoginResponse(
         String token,
+        String volunteerId,
         String email,
         String role
 ) {
