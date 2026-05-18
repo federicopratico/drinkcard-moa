55af1480e729e89dcecaff61d107ad151f08a536
federicopratico <federico.pratico@icloud.com>
2026-05-18 11:42:15 +0200

    feature: Expose me enpoint to retrieve current user infos

---
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/infrastructure/adapter/in/rest/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package cat.itacademy.s04.t02.n02.drinkcardmoa.iam.infrastructure.adapter.in.rest.mapper;
+
+import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.application.port.in.dto.command.CurrentUserCommand;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.application.port.in.dto.result.CurrentUserResult;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.infrastructure.adapter.in.rest.dto.response.CurrentUserResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public CurrentUserCommand toCommand(String email) {
+        return new CurrentUserCommand(email);
+    }
+
+    public CurrentUserResponse toResponse(CurrentUserResult result) {
+        return new CurrentUserResponse(
+                result.userId(),
+                result.fullName(),
+                result.email(),
+                result.role(),
+                result.status()
+        );
+    }
+}
