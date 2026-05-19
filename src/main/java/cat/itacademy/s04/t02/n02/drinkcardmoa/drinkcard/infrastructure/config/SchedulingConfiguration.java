46e8ffcbd3846698d93c5c76e9e9adeaaaed64e0
federicopratico <federico.pratico@icloud.com>
2026-05-19 20:04:43 +0200

    feature: Add periodic clean up of expire drink tickets

---
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/drinkcard/infrastructure/config/SchedulingConfiguration.java
@@ -0,0 +1,9 @@
+package cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfiguration {
+}
