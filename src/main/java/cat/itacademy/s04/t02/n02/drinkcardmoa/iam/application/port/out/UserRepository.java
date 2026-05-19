d0086965a14d258fe7d4803a9901fc9a41151a81
federicopratico <federico.pratico@icloud.com>
2026-05-19 23:26:10 +0200

    refactor: Change repository contract

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/application/port/out/UserRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/iam/application/port/out/UserRepository.java
@@ -1,9 +1,11 @@
 package cat.itacademy.s04.t02.n02.drinkcardmoa.iam.application.port.out;
 
+import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.application.port.out.query.UserSearchCriteria;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.domain.model.aggregate.User;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.domain.model.valueobject.Email;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.domain.model.valueobject.Role;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.iam.domain.model.valueobject.UserStatus;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.application.dto.PageResult;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.domain.VolunteerID;
 
 import java.util.List;
@@ -13,6 +15,6 @@ public interface UserRepository {
     User save(User user);
     Optional<User> findUserByEmail(Email email);
     boolean existsByEmail(Email email);
-    List<User> findAllByFilters(Role role, UserStatus status, Email email);
+    PageResult<User> searchUsers(UserSearchCriteria criteria);
     Optional<User> findById(VolunteerID volunteerID);
 }
