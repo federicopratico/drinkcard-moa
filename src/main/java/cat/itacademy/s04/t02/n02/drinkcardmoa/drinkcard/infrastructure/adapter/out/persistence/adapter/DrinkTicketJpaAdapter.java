a70d55106f533149c0a00134c300cb22f5ebb56e
federicopratico <federico.pratico@icloud.com>
2026-05-19 18:13:40 +0200

    feature: Add respository feature to check if exists another pending ticket for a user

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/drinkcard/infrastructure/adapter/out/persistence/adapter/DrinkTicketJpaAdapter.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/drinkcard/infrastructure/adapter/out/persistence/adapter/DrinkTicketJpaAdapter.java
@@ -3,11 +3,14 @@ package cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.infrastructure.adapter.
 import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.application.port.out.DrinkTicketRepository;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.domain.model.DrinkTicket;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.domain.model.DrinkTicketID;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.domain.model.DrinkTicketStatus;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.infrastructure.adapter.out.persistence.mapper.DrinkTicketMapper;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.infrastructure.adapter.out.persistence.repository.JpaDrinkTicketRepository;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.domain.VolunteerID;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Repository
@@ -26,5 +29,14 @@ public class DrinkTicketJpaAdapter implements DrinkTicketRepository {
     public Optional<DrinkTicket> findByDrinkTicketId(DrinkTicketID drinkTicketId) {
         return jpaDrinkTicketRepository.findByDrinkTicketId(drinkTicketId.value())
                 .map(mapper::toDomain);
+    }
+
+    @Override
+    public boolean existsActivePendingByVolunteerId(VolunteerID volunteerId, Instant now) {
+        return jpaDrinkTicketRepository.existsByVolunteerIdAndStatusAndExpiresAtAfter(
+                volunteerId.value(),
+                DrinkTicketStatus.PENDING.name(),
+                now
+        );
     }
 }
