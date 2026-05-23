02070c12db04b6df4666840a4eb238a6b3feefbb
federicopratico <federico.pratico@icloud.com>
2026-05-23 11:51:47 +0200

    refactor: Add search with filter method to repostiroy and its adapter

--- src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/drinkcard/application/port/out/DrinkCardAccountRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/drinkcard/application/port/out/DrinkCardAccountRepository.java
@@ -1,14 +1,15 @@
 package cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.application.port.out;
 
+import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.application.port.out.query.DrinkCardAccountSearchCriteria;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.application.dto.PageResult;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.domain.VolunteerID;
 import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.domain.model.DrinkCardAccount;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface DrinkCardAccountRepository {
     DrinkCardAccount save(DrinkCardAccount drinkCardAccount);
     Optional<DrinkCardAccount> findByVolunteerId(VolunteerID volunteerID);
     boolean existsByVolunteerId(VolunteerID volunteerID);
-    List<DrinkCardAccount> findAll();
+    PageResult<DrinkCardAccount> searchDrinkCardAccounts(DrinkCardAccountSearchCriteria criteria);
 }
---
+++ src/main/java/cat/itacademy/s04/t02/n02/drinkcardmoa/drinkcard/application/port/out/query/DrinkCardAccountSearchCriteria.java
@@ -0,0 +1,14 @@
+package cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.application.port.out.query;
+
+import cat.itacademy.s04.t02.n02.drinkcardmoa.drinkcard.domain.model.DrinkCardAccountStatus;
+import cat.itacademy.s04.t02.n02.drinkcardmoa.shared.domain.VolunteerID;
+
+public record DrinkCardAccountSearchCriteria(
+        VolunteerID volunteerId,
+        DrinkCardAccountStatus status,
+        int page,
+        int size,
+        String sortBy,
+        String sortDirection
+) {
+}
